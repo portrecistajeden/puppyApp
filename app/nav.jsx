0f63eee6f5a16ce1fe5e5925e1b7d14539b3de1a
wi104036 <piawramiuk@gmail.com>
2023-05-12 23:19:01 +0200

    przypomnialem sobie o top w position fixed

--- app/nav.jsx
+++ app/nav.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
     return (
-        <div className="fixed w-full bg-emerald-300 h-16">
+        <div className="fixed top-0 w-full bg-emerald-300 h-16">
             <Link href='/'>Home</Link>
             <Link href='/about'>About</Link>
         </div>
